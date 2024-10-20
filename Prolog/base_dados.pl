283a0f39d0d9dd9b4fac3437a1a6626540706483
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-10-19 23:30:11 -0300

    Create base_dados.pl

---
+++ Prolog/base_dados.pl
@@ -0,0 +1,36 @@
+/* Alguns homens */
+masc(felipe).
+masc(chico).
+masc(andre).
+masc(ted).
+masc(uil).
+masc(harris).
+
+/* Algumas mulheres */
+fem(bete).
+fem(mag).
+fem(ana).
+fem(di).
+fem(sara).
+fem(beatriz).
+fem(engenia).
+
+/* Quem é pai de quem */
+pai(felipe,chico).
+pai(felipe,ana).
+pai(felipe,andre).
+pai(felipe,ted).
+pai(chico,uil).
+pai(chico,harris).
+pai(andre,beatriz).
+pai(andre,eugenia).
+
+/* Quem é mãe de quem */
+mae(bete,chico).
+mae(bete,ana).
+mae(bete,andre).
+mae(bete,ted).
+mae(di,uil).
+mae(di,harris).
+mae(sara,beatriz).
+mae(sara,eugenia).
