47a9b9a10b2a15b30fc54f563e2f29b1b0e5b01d
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-10-20 01:08:59 -0300

    .

--- Conceitos de LPs/Compilador/Compilador.java
+++ Conceitos de LPs/Compilador/Compilador.java
@@ -5,17 +5,19 @@ import java.util.List;
 enum TipoToken {
     PRE_PROCESSADOR, 
 	BIBLIOTECA, 
-	ABRE_COL_ANG, 
-	FECHA_COL_ANG,  
+	ABRE_COLCHETE_ANGULADO, 
+	FECHA_COLCHETE_ANGULADO,  
 	TIPO, 
 	IDENTIFICADOR, 
 	ESPACO, 
-	ABRE_PARENT, 
-	FECHA_PARENT, 
+	ABRE_PARENTESE, 
+	FECHA_PARENTESE, 
 	ABRE_CHAVE, 
 	FECHA_CHAVE, 
 	LISTA_PARAMETROS, 
-	LISTA_COMANDOS, 
+	ATRIBUICAO, 
+	EXPRESSAO, 
+	FIM_INSTRUCAO, 
 	EOF
 }
 
@@ -66,10 +68,10 @@ class AnalisadorLexico {
                 tokens.add(new Token(TipoToken.BIBLIOTECA, "$BIBLIO$"));
                 posicao += "$BIBLIO$".length();
             } else if (entrada.startsWith("$ABR_COL_ANG$", posicao)) {
-                tokens.add(new Token(TipoToken.ABRE_COL_ANG, "$ABR_COL_ANG$"));
+                tokens.add(new Token(TipoToken.ABRE_COLCHETE_ANGULADO, "$ABR_COL_ANG$"));
                 posicao += "$ABR_COL_ANG$".length();
             } else if (entrada.startsWith("$FEC_COL_ANG$", posicao)) {
-                tokens.add(new Token(TipoToken.FECHA_COL_ANG, "$FEC_COL_ANG$"));
+                tokens.add(new Token(TipoToken.FECHA_COLCHETE_ANGULADO, "$FEC_COL_ANG$"));
                 posicao += "$FEC_COL_ANG$".length();
             } else if (entrada.startsWith("$TIPO$", posicao)) {
                 tokens.add(new Token(TipoToken.TIPO, "$TIPO$"));
@@ -81,25 +83,31 @@ class AnalisadorLexico {
                 tokens.add(new Token(TipoToken.ESPACO, "$ESP$"));
                 posicao += "$ESP$".length();
             } else if (entrada.startsWith("$ABR_PAR$", posicao)) {
-                tokens.add(new Token(TipoToken.ABRE_PARENT, "$ABR_PAR$"));
+                tokens.add(new Token(TipoToken.ABRE_PARENTESE, "$ABR_PAR$"));
                 posicao += "$ABR_PAR$".length();
             } else if (entrada.startsWith("@PARAMETROS@", posicao)) {
                 tokens.add(new Token(TipoToken.LISTA_PARAMETROS, "@PARAMETROS@"));
                 posicao += "@PARAMETROS@".length();				
             } else if (entrada.startsWith("$FEC_PAR$", posicao)) {
-                tokens.add(new Token(TipoToken.FECHA_PARENT, "$FEC_PAR$"));
+                tokens.add(new Token(TipoToken.FECHA_PARENTESE, "$FEC_PAR$"));
                 posicao += "$FEC_PAR$".length();
             } else if (entrada.startsWith("$ABR_CHA$", posicao)) {
                 tokens.add(new Token(TipoToken.ABRE_CHAVE, "$ABR_CHA$"));
                 posicao += "$ABR_CHA$".length();
-            } else if (entrada.startsWith("@COMANDOS@", posicao)) {
-                tokens.add(new Token(TipoToken.LISTA_COMANDOS, "@COMANDOS@"));
-                posicao += "@COMANDOS@".length();					
+            } else if (entrada.startsWith("$ATRIB$", posicao)) {
+                tokens.add(new Token(TipoToken.ATRIBUICAO, "$ATRIB$"));
+                posicao += "$ATRIB$".length();			
+            } else if (entrada.startsWith("@EXPRESSAO@", posicao)) {
+                tokens.add(new Token(TipoToken.EXPRESSAO, "@EXPRESSAO@"));
+                posicao += "@EXPRESSAO@".length();	
+            } else if (entrada.startsWith("$FIN_INST$", posicao)) {
+                tokens.add(new Token(TipoToken.FIM_INSTRUCAO, "$FIN_INST$"));
+                posicao += "$FIN_INST$".length();					
             } else if (entrada.startsWith("$FEC_CHA$", posicao)) {
                 tokens.add(new Token(TipoToken.FECHA_CHAVE, "$FEC_CHA$"));
                 posicao += "$FEC_CHA$".length();				
             } else {
-                throw new RuntimeException("Token inválido encontrado: " + entrada.charAt(posicao));
+                throw new RuntimeException("Token inválido encontrado: " + entrada.charAt(posicao) + "Posicao:" + posicao);
             }
         }
 
@@ -147,10 +155,10 @@ class AnalisadorSintatico {
 
     // <preprocessador> ::= <inclusao> <abre_colche_ang> <biblioteca> <fecha_colche_ang>
     private void verificarPreProcessador() {
-        consumir(TipoToken.PRE_PROCESSADOR);    // <inclusao>
-        consumir(TipoToken.ABRE_COL_ANG);       // <abre_colche_ang>
-        consumir(TipoToken.BIBLIOTECA);         // <biblioteca>
-        consumir(TipoToken.FECHA_COL_ANG);      // <fecha_colche_ang>
+        consumir(TipoToken.PRE_PROCESSADOR);         // <inclusao>
+        consumir(TipoToken.ABRE_COLCHETE_ANGULADO);  // <abre_colche_ang>
+        consumir(TipoToken.BIBLIOTECA);              // <biblioteca>
+        consumir(TipoToken.FECHA_COLCHETE_ANGULADO); // <fecha_colche_ang>
     }
 
     // <definicao_funcao> ::= <especificador_tipo> <espaco> <identificador> <abre_parentese> <lista_parametros> <fecha_parentese> <abre_chave> <lista_comandos> <fecha_chave>
@@ -158,24 +166,44 @@ class AnalisadorSintatico {
         consumir(TipoToken.TIPO);               // <especificador_tipo>
         consumir(TipoToken.ESPACO);             // <espaco>
         consumir(TipoToken.IDENTIFICADOR);      // <identificador>
-        consumir(TipoToken.ABRE_PARENT);        // <abre_parentese>
+        consumir(TipoToken.ABRE_PARENTESE);     // <abre_parentese>
         consumir(TipoToken.LISTA_PARAMETROS);   // <lista_parametros>		
-        consumir(TipoToken.FECHA_PARENT);       // <fecha_parentese>
+        consumir(TipoToken.FECHA_PARENTESE);    // <fecha_parentese>
         consumir(TipoToken.ABRE_CHAVE);         // <abre_chave>
-        consumir(TipoToken.LISTA_COMANDOS);     // <lista_comandos>			
+        verificarListaComandos();				// <lista_comandos>			
         consumir(TipoToken.FECHA_CHAVE);        // <fecha_chave>
+    }
+	// <lista_comandos> ::= ( <comando> )*
+    public void verificarListaComandos() {
+		verificarComandoDeclaracaoAtribuicao();
+	}
+	
+    // <comando_declaracao_atribuicao> ::= ( <especificador_tipo> <espaco> )? <identificador> <atribuicao> "@EXPRESSAO@" <fim_instrucao>
+    public void verificarComandoDeclaracaoAtribuicao() {
+        consumir(TipoToken.TIPO);               // <especificador_tipo>
+        consumir(TipoToken.ESPACO);             // <espaco>
+        consumir(TipoToken.IDENTIFICADOR);		// <identificador>
+        consumir(TipoToken.ATRIBUICAO); 		// <atribuicao>
+        consumir(TipoToken.EXPRESSAO);			// <expressao>
+        consumir(TipoToken.FIM_INSTRUCAO);		// <fim_instrucao>
     }	
 }
 
 // Classe principal do compilador
 public class Compilador {
     public static void main(String[] args) {
-        String entrada = "$PRE_PROC$ $ABR_COL_ANG$ $BIBLIO$ $FEC_COL_ANG$ $PRE_PROC$ $ABR_COL_ANG$ $BIBLIO$ $FEC_COL_ANG$"  +
-			"$TIPO$ $ESP$ $IDENT$ $ABR_PAR$ @PARAMETROS@ $FEC_PAR$ $ABR_CHA$ @COMANDOS@ $FEC_CHA$";
+        String entrada = "$PRE_PROC$ $ABR_COL_ANG$ $BIBLIO$ $FEC_COL_ANG$ " +
+						 "$PRE_PROC$ $ABR_COL_ANG$ $BIBLIO$ $FEC_COL_ANG$ " +
+						 "$TIPO$ $ESP$ $IDENT$ $ABR_PAR$ @PARAMETROS@ $FEC_PAR$ $ABR_CHA$ " +
+						 "$TIPO$ $ESP$ $IDENT$ $ATRIB$ @EXPRESSAO@ $FIN_INST$ " +
+						 "$FEC_CHA$";
         
         AnalisadorLexico analizadorLexico = new AnalisadorLexico(entrada);
         List<Token> tokens = analizadorLexico.tokenizar();
 
+		for(Token token: tokens)
+			System.out.println(token.tipo);
+			
         AnalisadorSintatico analizadorSintatico = new AnalisadorSintatico(tokens);
 		try {
 			analizadorSintatico.verificarPrograma();
