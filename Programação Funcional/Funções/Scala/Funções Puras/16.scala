5617cd1d6d9905a2b5f3c5c70f44c9ef319575a2
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-11-07 14:13:54 -0300

    Update 16.scala

--- Programação Funcional/Funções/Scala/Funções Puras/16.scala
+++ Programação Funcional/Funções/Scala/Funções Puras/16.scala
@@ -5,11 +5,20 @@ object Main {
     //Aqui, (2 to math.sqrt(n).toInt) cria um intervalo de números começando em 2 até o valor da raiz quadrada de n convertido para inteiro.
     //O método forall testa se a condição n % x != 0 (ou seja, n não é divisível por x) é true para todos os valores de x no intervalo.
     //Se n não for divisível por nenhum dos números entre 2 e a raiz quadrada de n, então forall retorna true, indicando que n é primo.
+	//forall é a função inversa da exists
     else (2 to math.sqrt(n).toInt).forall(x => n % x != 0) // Verifica se n não é divisível por nenhum número até a raiz quadrada de n
   }
 
   // Exemplos de uso
   def main(args: Array[String]): Unit = {
+	//println((0 to 10).mkString(" "))
+	
+	//(0 to 10).foreach(println)
+	
+	range(intervalo_fechado, intervalo_aberto)
+	//val lista = List.range(0, 11)
+    //println(lista.mkString(" "))
+	
     println(isPrimo(2))   // true
     println(isPrimo(3))   // true
     println(isPrimo(4))   // false
