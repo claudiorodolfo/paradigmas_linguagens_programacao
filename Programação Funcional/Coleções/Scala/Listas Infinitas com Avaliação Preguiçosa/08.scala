27d38063c8574b7e0b76cceda1d2a861f0bd2908
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-11-11 03:54:42 -0300

    .

--- Programação Funcional/Coleções/Scala/Listas Infinitas com Avaliação Preguiçosa/08.scala
+++ Programação Funcional/Coleções/Scala/Listas Infinitas com Avaliação Preguiçosa/08.scala
@@ -1,6 +1,6 @@
 object Main extends App {
   // Define uma lista infinita de números de Fibonacci
-  lazy val numerosFibonacci: LazyList[Int] = 0 #:: 1 #:: numerosFibonacci.zip(numerosFibonacci.tail).map { case (a, b) => a + b }
+  lazy val numerosFibonacci: LazyList[BigInt] = BigInt(0) #:: BigInt(1) #:: numerosFibonacci.zip(numerosFibonacci.tail).map { case (a, b) => a + b }
 
   // Obtém os primeiros 10 números de Fibonacci e os converte em uma List
   val primeiros10Fibonacci = numerosFibonacci.take(10).toList
