d2c10b49bfabe930b65b4753a320c4515fe87581
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2024-12-19 05:15:28 -0300

    Update 15.pl

--- Programação Lógica/Prolog/Listas/15.pl
+++ Programação Lógica/Prolog/Listas/15.pl
@@ -1,6 +1,6 @@
 soma_n(_, 0, 0).  % Caso base
-soma_n([Cabeça | Cauda], N, Soma) :-
+soma_n([Cabeca | Cauda], N, Soma) :-
     N > 0, 
     N1 is N - 1,
     soma_n(Cauda, N1, SomaCauda),
-    Soma is Cabeça + SomaCauda.
+    Soma is Cabeca + SomaCauda.
