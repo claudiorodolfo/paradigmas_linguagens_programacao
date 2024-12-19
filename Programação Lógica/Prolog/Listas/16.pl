12c4603d15289d439c4f58dd39bf897f561ca97c
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-12-19 18:51:01 -0300

    Update 16.pl

--- Programação Lógica/Prolog/Listas/16.pl
+++ Programação Lógica/Prolog/Listas/16.pl
@@ -1,6 +1,12 @@
+% Predicado para verificar se um número é par
+par(X) :- X mod 2 =:= 0.
+
+% Predicado para verificar se um número é maior que 5
+maior_que_5(X) :- X > 5.
+
 filtrar([], _, []).
-filtrar([Cabeça | Cauda], Predicado, [Cabeca | Resultado]) :-
+filtrar([Cabeca | Cauda], Predicado, [Cabeca | Resultado]) :-
     call(Predicado, Cabeca),
     filtrar(Cauda, Predicado, Resultado).
-filtrar([_|Cauda], Predicado, Resultado) :-
+filtrar([_ | Cauda], Predicado, Resultado) :-
     filtrar(Cauda, Predicado, Resultado).
