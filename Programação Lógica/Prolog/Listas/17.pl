8931b2133db5c68c62c793ac12cf3def9f69d47e
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-12-19 14:43:00 -0300

    prolog

--- Programação Lógica/Prolog/Listas/17.pl
+++ Programação Lógica/Prolog/Listas/17.pl
@@ -1,6 +1,8 @@
 pares([], []).
-pares([Cabeça | Cauda], [Cabeça | Resultado]) :-
-    0 is Cabeça mod 2, 
+
+pares([Cabeca | Cauda], [Cabeca | Resultado]) :-
+    0 is Cabeca mod 2, 
     pares(Cauda, Resultado).
+    
 pares([_|Cauda], Resultado) :-
     pares(Cauda, Resultado).
