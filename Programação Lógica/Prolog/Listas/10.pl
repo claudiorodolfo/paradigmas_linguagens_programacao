8b828633fa9456405cf91c07f6cd020a05e4ad04
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2024-12-19 05:12:55 -0300

    Update 10.pl

--- Programação Lógica/Prolog/Listas/10.pl
+++ Programação Lógica/Prolog/Listas/10.pl
@@ -1,4 +1,4 @@
 produto_lista([], 1).  % Caso base: o produto de uma lista vazia é 1
-produto_lista([Cabeça | Cauda], Produto) :- 
+produto_lista([Cabeca | Cauda], Produto) :- 
     produto_lista(Cauda, ProdutoCauda), 
-    Produto is Cabeça * ProdutoCauda.
+    Produto is Cabeca * ProdutoCauda.
