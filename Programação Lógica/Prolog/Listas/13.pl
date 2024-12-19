7026c42257007c4712021974ae0d55c89f0b42fb
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2024-12-19 05:11:59 -0300

    Update 13.pl

--- Programação Lógica/Prolog/Listas/13.pl
+++ Programação Lógica/Prolog/Listas/13.pl
@@ -1,7 +1,7 @@
 remover_duplicatas([], []).  % Caso base
-remover_duplicatas([Cabeça | Cauda], [Cabeça | Resultado]) :-
-    \+ membro(Cabeça, Cauda),  % Verifica se Cabeça não está na Cauda
+remover_duplicatas([Cabeca | Cauda], [Cabeca | Resultado]) :-
+    \+ membro(Cabeca, Cauda),  % Verifica se Cabeça não está na Cauda
     remover_duplicatas(Cauda, Resultado).
-remover_duplicatas([Cabeça | Cauda], Resultado) :-
-    membro(Cabeça, Cauda),  % Caso recursivo: se Cabeça está em Cauda
+remover_duplicatas([Cabeca | Cauda], Resultado) :-
+    membro(Cabeca, Cauda),  % Caso recursivo: se Cabeça está em Cauda
     remover_duplicatas(Cauda, Resultado).
