dac3fc68c69c64d6ca6c41e79025adcecbaa5f99
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2024-12-27 23:33:36 -0300

    Update 13.pl

--- Programação Lógica/Prolog/Listas/13.pl
+++ Programação Lógica/Prolog/Listas/13.pl
@@ -1,7 +1,14 @@
+pertence(X, [X | _]).  % Caso base: elemento é a cabeça
+pertence(X, [_ | Cauda]) :- 
+    pertence(X, Cauda).  % Caso recursivo
+nao_pertence(_, []).  % Caso base: elemento não está em uma lista vazia
+nao_pertence(X, [Y | Cauda]) :-
+    X \= Y,  % X é diferente de Y
+    nao_pertence(X, Cauda).
 remover_duplicatas([], []).  % Caso base
 remover_duplicatas([Cabeca | Cauda], [Cabeca | Resultado]) :-
-    \+ membro(Cabeca, Cauda),  % Verifica se Cabeça não está na Cauda
+    nao_pertence(Cabeca, Cauda),  % Verifica se Cabeça não está na Cauda
     remover_duplicatas(Cauda, Resultado).
 remover_duplicatas([Cabeca | Cauda], Resultado) :-
-    membro(Cabeca, Cauda),  % Caso recursivo: se Cabeça está em Cauda
+    pertence(Cabeca, Cauda),  % Caso recursivo: se Cabeça está em Cauda
     remover_duplicatas(Cauda, Resultado).
