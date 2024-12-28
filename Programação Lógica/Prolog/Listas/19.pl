ca7694cfd0279f2bf82cd3efc842a9828d55ec05
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2024-12-27 23:56:11 -0300

    Update 19.pl

--- Programação Lógica/Prolog/Listas/19.pl
+++ Programação Lógica/Prolog/Listas/19.pl
@@ -1,5 +1,9 @@
 dividir_meio(Lista, PrimeiraMetade, SegundaMetade) :-
-    length(Lista, Tamanho),
-    Meio is Tamanho // 2,
-    length(PrimeiraMetade, Meio),
-    append(PrimeiraMetade, SegundaMetade, Lista).
+    length(Lista, Comprimento),         % Obtém o comprimento da lista
+    Meio is Comprimento // 2,           % Calcula o ponto de divisão (metade inteira)
+    dividir(Lista, Meio, PrimeiraMetade, SegundaMetade).
+% Caso base: nenhuma divisão restante
+dividir(Lista, 0, [], Lista).
+% Divide a lista recursivamente
+dividir([Cabeca | Cauda], N, [Cabeca | Primeira], Segunda) :-
+    N > 0, N1 is N - 1, dividir(Cauda, N1, Primeira, Segunda).
